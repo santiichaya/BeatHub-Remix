98ce6ee707b184f449ee057bec8778aebbaba3a6
MarioMunilla <mariomunillaperez@gmail.com>
2024-12-23 20:28:29 +0100

    Implementada API Spotify. Se crea carpeta .server(spotify.ts). Refactorizacion en carpeta models(artist.server.ts). En carpeta routes/search.tsx, se hace prueba para buscar artista

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 import HomeButton from './HomeButton.js';
 import SearchButton from './SearchButton.js';
 
+import logo from '../assets/onlyLogo.png'
+
 function Header() {
     return (
-        <div className="fixed bg-header h-full w-auto top-0 left-0 z-1000 flex flex-col items-center justify-between p-4">
+        <div className="fixed  bg-header h-full w-auto top-0 left-0 z-1000 flex flex-col items-center justify-between p-4">
             <Link to="/UserPage" className="flex flex-col items-center space-y-2">
-                <img className="w-[3.75rem] h-[3.75rem]" src="app/assets/onlyLogo.png" alt="logo" />
+                <img className="w-[3.75rem] h-[3.75rem]" src={logo} alt="logo" />
                 <span className="font-kanit font-bold">beathub</span>
             </Link>
             <div className="flex flex-col items-center space-y-10 mb-8">
