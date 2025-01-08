eeeec1992a68bf195b049d5bf6457141c1eef613
jodric10 <jorgemodric8@gmail.com>
2025-01-08 19:05:24 +0100

    Creo una función que gestiona la lógica al eliminar un elemento de la base de datos, devolviendo true en caso de éxito o un mensaje de error si el registro no existe (P2025). Otros errores son lanzados para su manejo posterior.

--- app/models/utils.ts
+++ app/models/utils.ts
@@ -1,15 +1,15 @@
 import { Prisma } from "@prisma/client";
 
-export function handleDelete<T>(deleteFn:()=>T){
+export async function handleDelete<T>(deleteFn:()=>Promise<T>): Promise<string|boolean>{
     try{
-        const deleted=deleteFn;
-        return deleted;
+        await deleteFn();
+        return true;
     }catch(error){
         if(error instanceof Prisma.PrismaClientKnownRequestError){
             if(error.code==="P2025"){
                 return error.message;
             }
-            throw error;
         }
+        throw error;
     }
 }
