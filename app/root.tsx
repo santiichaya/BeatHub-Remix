6c7640c9b04ae398a2229f13db4ee2e4a37963d4
jodric10 <jorgemodric8@gmail.com>
2025-01-15 09:55:08 +0100

    cambiado root

--- app/root.tsx
+++ app/root.tsx
@@ -3,41 +3,6 @@ import type { ErrorResponse, LinksFunction } from "@remix-run/node";
 
 import tailwindCSSURL from "./tailwind.css?url";
 import Header from "./components/Header";
-
-export const links: LinksFunction = () => [
-  {rel:"stylesheet",href:tailwindCSSURL},
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
-export function Layout({children}:{children: React.ReactNode}) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="bg-primary flex">
-          <nav>
-            <Header />
-          </nav>
-          <h1 className="titulo"></h1>
-          {children}
-      </body>import { Links, Meta, Outlet, Scripts, useRouteError } from "@remix-run/react";
-import type { ErrorResponse, LinksFunction } from "@remix-run/node";
-
-import tailwindCSSURL from "./tailwind.css?url";
-import Header from "./components/Header";
 import { Container } from "postcss";
 import { ContainerPlayList } from "./components/ContainerPlayList.tsx";
 
@@ -73,49 +38,6 @@ export function Layout({children}:{children: React.ReactNode}) {
           </main>
           <Scripts/>
       </body>
-    </html>
-  );
-}
-
-export default function App() {
-  return <Outlet />;
-}
-
-export function ErrorBoundary() {
-  const error = useRouteError() as ErrorResponse;
-
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div className="min-h-screen flex items-center justify-center bg-primary">
-          <div className="text-center p-8 bg-secondary rounded-lg shadow-md">
-            <h1 className="text-6xl font-bold text-gray-100 mb-4">404</h1>
-            <h2 className="text-2xl font-semibold text-gray-100 mb-4">
-              ¡Página no encontrada!
-            </h2>
-            {error.status == 404 && 
-              <p className="text-black mb-8">
-                Lo sentimos, la página que estás buscando no existe.
-              </p>
-            }
-            <a
-              href="/"
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-            >
-              Volver al inicio
-            </a>
-          </div>
-        </div>
-      </body>
-    </html>
-  );
-}
     </html>
   );
 }
