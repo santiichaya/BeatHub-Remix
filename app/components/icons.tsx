a8b98e24343b090c39e535b8efab6539d89766c3
jodric10 <jorgemodric8@gmail.com>
2025-01-19 01:53:52 +0100

    Sigo con la validación de register.

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -1,7 +1,7 @@
-export function DeleteIcon(){
-    return(
+export function DeleteIcon() {
+    return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
     )
 }
@@ -22,3 +22,16 @@ export function CloseEyeIcon() {
         </svg>
     )
 }
+
+export function AcceptedIcon() {
+    return (
+        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.8160000000000001"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM16.0303 8.96967C16.3232 9.26256 16.3232 9.73744 16.0303 10.0303L11.0303 15.0303C10.7374 15.3232 10.2626 15.3232 9.96967 15.0303L7.96967 13.0303C7.67678 12.7374 7.67678 12.2626 7.96967 11.9697C8.26256 11.6768 8.73744 11.6768 9.03033 11.9697L10.5 13.4393L12.7348 11.2045L14.9697 8.96967C15.2626 8.67678 15.7374 8.67678 16.0303 8.96967Z" fill="#00ff1e"></path> </g></svg>
+    )
+}
+
+export function RejectedIcon() {
+    return (
+        <svg width="30px" height="30px" viewBox="-4.96 -4.96 25.92 25.92" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.592"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM4.29289 5.70711L6.58579 8L4.29289 10.2929L5.70711 11.7071L8 9.41421L10.2929 11.7071L11.7071 10.2929L9.41421 8L11.7071 5.70711L10.2929 4.29289L8 6.58579L5.70711 4.29289L4.29289 5.70711Z" fill="#ff0000"></path> </g></svg>
+    )
+}
+
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,40 +1,44 @@
 import { ActionFunction, redirect } from "@remix-run/node";
 import { NavLink, useFetcher } from "@remix-run/react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { z } from "zod";
-import { CloseEyeIcon, OpenEyeIcon } from "~/components/icons";
+import { AcceptedIcon, CheckedIcon, CloseEyeIcon, OpenEyeIcon, RejectedIcon } from "~/components/icons";
 import { createUser, getUserByUsername } from "~/models/user.server";
 import { generate_hash } from "~/utils/hash";
 import { commitSession, getSession } from "~/utils/session";
 
 const badWords = [
-  "polla", "coño", "puta", "gilipollas", "cabrón", "mierda",
-  "zorra", "maricón", "puto", "imbécil", "estúpido", "culo",
-  "cojones", "follar"];
+    "polla", "coño", "puta", "gilipollas", "cabrón", "mierda",
+    "zorra", "maricón", "puto", "imbécil", "estúpido", "culo",
+    "cojones", "follar"];
 
 //Función para eliminar tildes
 const removeAccents = (str: string) => {
-  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); //Normalize con el valor NFD lo que hace es tratar a las tildes 
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); //Normalize con el valor NFD lo que hace es tratar a las tildes 
 };
 
-const RegisterSchema = z.object({
-  username: z.string().min(1, "No puede estar vacío el campo"),
-  email: z.string().min(1, "No puedes dejar vacío el email").email(),
-  password: z.string().min(1, "No puedes dejarlo vacío")
-    .refine((password) => password.length<=16, { message: "Debe tener al menos 8 caracteres" })
+
+const passwordSchema = z.string().min(8, { message: "Debe tener al menos 8 caracteres" })
+    .max(16, { message: "No debe sobrepasar los 16 caracteres" })
     .refine((password) => /[A-Z]/.test(password), { message: "Debe tener al menos una mayúscula" })
     .refine((password) => /\d/.test(password), { message: "Debe tener al menos un número" })
     .refine((password) => !/\s/.test(password), { message: "No debe contener espacios" })
     .refine((password) => !["123456", "password", "qwerty"].includes(password), { message: "La contraseña es demasiado común" })
     .transform((password) => removeAccents(password.toLowerCase()))
-    .refine((password) => !badWords.some((word: string) => password.includes(word)), { message: "No utilices palabras malsonantes" })
+    .refine((password) => !badWords.some((word: string) => password.includes(word)), { message: "No utilices palabras malsonantes" });
+
+const RegisterSchema = z.object({
+    username: z.string().min(1, "No puede estar vacío el campo"),
+    email: z.string().min(1, "No puedes dejar vacío el email").email(),
+    password: passwordSchema,
 });
 
 
+
 export const action: ActionFunction = async ({ request }) => {
-        const cookieHeader= request.headers.get("cookie"); //Recojo la cookie asociada a la sesión.
-        const session= await getSession(cookieHeader); //Obtengo la sesión.
-        console.log(session);
+    const cookieHeader = request.headers.get("cookie"); //Recojo la cookie asociada a la sesión.
+    const session = await getSession(cookieHeader); //Obtengo la sesión.
+    console.log(session);
     try {
         const datosFormulario = await request.formData();
         const username = datosFormulario.get("username") as string;
@@ -47,15 +51,15 @@ export const action: ActionFunction = async ({ request }) => {
             const passwordHash = await generate_hash(password);
             await createUser(username, passwordHash, email);
             console.log("Antes de obtenerlo:", user);
-            user=await getUserByUsername(username);
+            user = await getUserByUsername(username);
             console.log(user);
-            session.set("userId",user!.id);
-            session.set("username",user!.username);
-            return redirect("/",{
-                headers:{
+            session.set("userId", user!.id);
+            session.set("username", user!.username);
+            return redirect("/", {
+                headers: {
                     "Set-Cookie": await commitSession(session) //Confirmar cambios
                 }
-            }); 
+            });
         }
         return null
 
@@ -66,14 +70,32 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function Register() {
+    //Quiero hacer un array con todas las validaciones del passwordSchema
+    const passwordValidations = [
+        "Debe tener al menos 8 caracteres",
+        "No debe sobrepasar los 16 caracteres",
+        "Debe tener al menos una mayúscula",
+        "Debe tener al menos un número",
+        "No debe contener espacios",
+        "La contraseña es demasiado común",
+        "No utilices palabras malsonantes",
+    ];
     const fetcherRegister = useFetcher();
     const [password, setPassword] = useState<string>("");
     const [inputType, setInputType] = useState<string>('password');
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+    const [passwordErrors,setPasswordErrors]=useState<Array<string>>(["Debe tener al menos 8 caracteres","Debe tener al menos una mayúscula","Debe tener al menos un número"]);
 
-  function validatePassword(event: React.ChangeEvent<HTMLInputElement>) {
-
-    setPassword(event.target.value);
-  }
+    function validatePassword(event: React.ChangeEvent<HTMLInputElement>) {
+        const validation = passwordSchema.safeParse(event.target.value);
+        if(!validation.success){
+            const erroresdeZod=validation.error?.errors.map(er=>er.message); //Extraigo solo los mensajes de error para saber cuales son y los meto en el estado. validation.error es un objeto error que tiene una propiedad errors que es un array de objetos que contiene todos los errores, donde cada objeto representa un error de zod y por último, lo que hago es un map de errors para poder sacar cada mensaje de error de cada objeto.
+            setPasswordErrors(erroresdeZod);
+        }else{
+            setPasswordErrors([]);
+        }
+        setPassword(event.target.value);
+    }
     return (
         <div className="w-full h-full flex flex-col justify-center items-center gap-20">
             <h1 className='text-4xl justify-self-start'>Registrate!</h1>
@@ -85,8 +107,23 @@ export default function Register() {
                     <input type="email" name="email" placeholder="ejemplo@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,}$" className='w-[60%] border-2 rounded border-slate-200 focus:outline-none p-1 h-12' />
                 </label>
                 <label className='mt-6 w-full flex justify-between relative border-b border-black pb-4 items-center'>Contraseña:
-                <input type={inputType} name="password" value={password} onChange={validatePassword} className='w-[60%] border-2 rounded border-slate-200 focus:outline-none p-1 h-12' />
-                    <button type="button" className='absolute right-[10px]' onMouseDown={() => setInputType("text")} onMouseLeave={() => setInputType("password")} onMouseUp={() => setInputType("password")}>{inputType == "text" ? (<OpenEyeIcon />) : (<CloseEyeIcon />)}</button>
+                    <div className="relative w-[60%]">
+                        <input type={inputType} name="password" value={password} onChange={validatePassword} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} className="w-full border-2 rounded border-slate-200 focus:outline-none p-1 h-12" />
+                        <button type="button" className="absolute right-2 top-1/2 transform -translate-y-1/2"
+                            onMouseDown={() => setInputType("text")} onMouseLeave={() => setInputType("password")} onMouseUp={() => setInputType("password")}>{inputType == "text" ? (<OpenEyeIcon />) : (<CloseEyeIcon />)}
+                        </button>
+                        {isFocused && (
+                            <div className="absolute top-[110%] left-0 w-full bg-gray-100 border border-gray-300 rounded-lg p-2 shadow-lg">
+                                <ul className="text-[0.6rem] text-gray-700">
+                                    {passwordValidations.map((validation, index) => (
+                                            <li key={index} className="flex flex-col gap-2">{validation}
+                                            {passwordErrors.includes(validation)?(<RejectedIcon/>):(<AcceptedIcon />)}
+                                            </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
                 </label>
                 <button className='mt-12 border-4 p-1 rounded-lg bg-slate-200 text-black w-[12rem]'>Registrarme</button>
             </fetcherRegister.Form>
