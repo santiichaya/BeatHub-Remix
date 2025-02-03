f57fd5606193154b8fcedb74705503a491d87a85
jodric10 <jorgemodric8@gmail.com>
2025-02-03 13:07:44 +0100

    ruta dinamica playlist y correccion de rutas

---
+++ app/routes/inicio.tsx
@@ -0,0 +1,35 @@
+import { LoaderFunction } from "@remix-run/node";
+import { Outlet } from "@remix-run/react";
+import { getSpotifyAdminToken } from "~/.server/spotify";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const offset = parseInt(url.searchParams.get("offset") || "0");
+  const token = await getSpotifyAdminToken();
+
+  const artistResponse = await fetch(
+    `https://api.spotify.com/v1/search?q=genre:pop&type=artist&limit=6&offset=${offset}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const artistData = await artistResponse.json();
+  const playlistAdmin = await fetch(
+    "https://api.spotify.com/v1/me/playlists?limit=6",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const playListData = await playlistAdmin.json();
+  return { artists: artistData.artists.items, offset, playlists: playListData.items };
+};
+
+export default function InicioLayout() {
+  return (
+      <Outlet/>
+  );
+}
